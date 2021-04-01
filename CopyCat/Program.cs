c7244815818fec6aed4932ceeef5419ed8a21b44
YOUSHA ARIF <youshaarif13@gmail.com>
2021-03-31 23:24:59 -0700

    Add project files.

---
+++ CopyCat/Program.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace CopyCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region example
+
+            var path = @"test.html";
+
+            var doc = new HtmlDocument();
+            doc.Load(path);
+
+            var node = doc.DocumentNode.SelectSingleNode("//body");
+
+            Console.WriteLine(node.OuterHtml);
+
+            #endregion
+        }
+    }
+}
